d6b55896d0b29ec5cd5a11a48943b7ad560d0d05
Spark4545 <lerouxpaulhenri@gmail.com>
2023-08-28 12:08:36 +0000

    task 0 with new insane thing

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -16,4 +16,4 @@ async def async_generator() -> Generator[float, None, None]:
     """
     for _ in range(10):
         await asyncio.sleep(1)
-        n = random.uniform(0, 10)
+        yield random.uniform(0, 10)
