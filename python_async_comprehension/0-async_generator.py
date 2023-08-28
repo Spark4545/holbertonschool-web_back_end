a29bdf79100a8a32e26d1d07989cfe89f7cda7db
Spark4545 <lerouxpaulhenri@gmail.com>
2023-08-28 12:05:48 +0000

    task 0 and README

---
+++ python_async_comprehension/0-async_generator.py
@@ -0,0 +1,19 @@
+    #!/usr/bin/env python3
+""" Async Generator """
+
+import asyncio
+import random
+from typing import Generator
+
+
+async def async_generator() -> Generator[float, None, None]:
+    """
+    Args:
+        None
+
+    Returns:
+        float random
+    """
+    for _ in range(10):
+        await asyncio.sleep(1)
+        n = random.uniform(0, 10)
