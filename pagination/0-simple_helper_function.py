8bc2aca8182d60ba70021bfb262fb5ec8be0e67c
Spark4545 <lerouxpaulhenri@gmail.com>
2023-09-04 23:11:11 +0200

    task 0 and 1

---
+++ pagination/0-simple_helper_function.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""0. Simple helper function"""
+from typing import Tuple
+
+
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
+    """
+    page: number of pages
+    page_size: size of each page
+    """
+    start = (page - 1) * page_size
+    end = page * page_size
+    return(start, end)
