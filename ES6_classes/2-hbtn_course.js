1c735d92e53c1b3555dd81a498e4fec99627057c
Spark4545 <lerouxpaulhenri@gmail.com>
2023-09-15 19:45:44 +0200

    task 2

---
+++ ES6_classes/2-hbtn_course.js
@@ -0,0 +1,44 @@
+/* eslint-disable class-methods-use-this */
+export default class HolbertonCourse {
+  constructor(name, length, students) {
+    this._name = this.validateString(name, 'Name');
+    this._length = this.validateNumber(length, 'Length');
+    this._students = this.validateArray(students, 'Students');
+  }
+  get name() {
+    return this._name;
+  }
+  set name(newName) {
+    this._name = this.validateString(newName, 'Name');
+  }
+  get length() {
+    return this._length;
+  }
+  set length(newLength) {
+    this._length = this.validateNumber(newLength, 'Length');
+  }
+  get students() {
+    return this._students;
+  }
+  set students(newStudents) {
+    this._students = this.validateArray(newStudents, 'Students');
+  }
+  validateString(value, attributeName) {
+    if (typeof value !== 'string') {
+      throw new TypeError(`${attributeName} must be a string`);
+    }
+    return value;
+  }
+  validateNumber(value, attributeName) {
+    if (typeof value !== 'number' || Number.isNaN(value)) {
+      throw new TypeError(`${attributeName} must be a number`);
+    }
+    return value;
+  }
+  validateArray(value, attributeName) {
+    if (!Array.isArray(value)) {
+      throw new TypeError(`${attributeName} must be an array`);
+    }
+    return value;
+  }
+}
