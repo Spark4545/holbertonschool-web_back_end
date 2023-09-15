4265421598eb164972412fb0f8e2e30d094d15e2
Spark4545 <lerouxpaulhenri@gmail.com>
2023-09-15 19:47:51 +0200

    task 2 lint

--- ES6_classes/2-hbtn_course.js
+++ ES6_classes/2-hbtn_course.js
@@ -5,36 +5,45 @@ export default class HolbertonCourse {
     this._length = this.validateNumber(length, 'Length');
     this._students = this.validateArray(students, 'Students');
   }
+
   get name() {
     return this._name;
   }
+
   set name(newName) {
     this._name = this.validateString(newName, 'Name');
   }
+
   get length() {
     return this._length;
   }
+
   set length(newLength) {
     this._length = this.validateNumber(newLength, 'Length');
   }
+
   get students() {
     return this._students;
   }
+
   set students(newStudents) {
     this._students = this.validateArray(newStudents, 'Students');
   }
+
   validateString(value, attributeName) {
     if (typeof value !== 'string') {
       throw new TypeError(`${attributeName} must be a string`);
     }
     return value;
   }
+
   validateNumber(value, attributeName) {
     if (typeof value !== 'number' || Number.isNaN(value)) {
       throw new TypeError(`${attributeName} must be a number`);
     }
     return value;
   }
+  
   validateArray(value, attributeName) {
     if (!Array.isArray(value)) {
       throw new TypeError(`${attributeName} must be an array`);
