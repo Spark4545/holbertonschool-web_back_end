332347b6c7ead05281e6a86654628b912319b62b
Spark4545 <lerouxpaulhenri@gmail.com>
2023-10-02 18:37:48 +0000

    quotes on success

--- ES6_promise/2-then.js
+++ ES6_promise/2-then.js
@@ -2,7 +2,7 @@ function handleResponseFromAPI(promise) {
   return promise
     .then(() => ({
       status: 200,
-      body: success,
+      body: 'success',
     }))
     .catch(() => Error())
     .finally(() => console.log('Got a response from the API'));
